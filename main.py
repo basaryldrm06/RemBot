2840063e149051a8470b03f4d756dd4e6245452e
basaryldrm06 <basaryldrm06@gmail.com>
2024-01-27 21:10:48 +0300

    global variables are added to main.py

--- main.py
+++ main.py
@@ -13,3 +13,24 @@ client = Client(api_key, secret_key)
 
 # csv path initialization
 csv_path_result = "./data/rembot_result.csv"
+
+# Global Variables
+indicator_check = None
+tp_count = 0
+sl_count = 0
+tp_price = 0
+sl_price = 0
+do_not_enter_long = False
+do_not_enter_short = False
+on_long = False
+on_short = False
+
+
+# initialization
+indicator_check = fetch_all_indicators(client)
+if (indicator_check.price < indicator_check.ema_100):
+    do_not_enter_long = True
+    print_with_color("yellow", "LONG BLOCKED")
+else:
+    do_not_enter_short = True
+    print_with_color("yellow", "SHORT BLOCKED")
