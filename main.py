aa020eb47afc9e2ed5efac2a42615883567676f2
basaryldrm06 <basaryldrm06@gmail.com>
2024-01-27 21:37:28 +0300

    result.csv is added & main.py is completed

--- main.py
+++ main.py
@@ -24,6 +24,39 @@ do_not_enter_long = False
 do_not_enter_short = False
 on_long = False
 on_short = False
+date = None
+
+# Global Functions
+def close_position(isTP):
+    global on_long
+    global on_short
+    global tp_count
+    global sl_count
+    global csv_path_result
+    global date
+
+    state = "" 
+    
+    if (on_long and isTP) or (on_short and (not isTP)): 
+        state = "LONG"
+    elif (on_long and (not isTP)) or (on_short and isTP):
+        state = "SHORT"
+
+    save_result(csv_path_result, date, state, "LONG" if on_long else "SHORT")
+
+    on_long = False
+    on_short = False
+
+    if isTP:
+        tp_count = tp_count + 1
+        print_with_color("green", "Position closed with TP")
+        print_with_color("yellow", "TP: " + str(tp_count) + " SL: " + 
+              str(sl_count) + " Win-Rate: " + calculateWR(tp_count, sl_count))
+    else:
+        sl_count = sl_count + 1
+        print_with_color("red", "Position closed with SL")
+        print_with_color("yellow", "TP: " + str(tp_count) + " SL: " + 
+            str(sl_count) + " Win-Rate: " + calculateWR(tp_count, sl_count))
 
 print_with_color("cyan", "MeraBot is running...")
 
@@ -47,7 +80,9 @@ while True:
                 (indicator_check.price < indicator_check.ema_100):
                 do_not_enter_short = False
                 do_not_enter_long = True
+                date = indicator_check.date
                 tp_price, sl_price = enter_long(client)
+                print()
                 print_with_color("yellow", "Entered LONG Current: " + 
                     str(round(indicator_check.price, 2)) + " TP_PRICE: " + str(round(tp_price, 2)) + 
                     " SL_PRICE: " + str(round(sl_price, 2)))
@@ -58,7 +93,9 @@ while True:
                 (indicator_check.price > indicator_check.ema_100):
                 do_not_enter_long = False
                 do_not_enter_short = True
+                date = indicator_check.date
                 tp_price, sl_price = enter_short(client)
+                print()
                 print_with_color("yellow", "Entered SHORT Current: " + 
                     str(round(indicator_check.price, 2)) + " TP_PRICE: " + str(round(tp_price, 2)) + 
                     " SL_PRICE: " + str(round(sl_price, 2)))
